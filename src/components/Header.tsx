90ab4ee034118ce7ce6fad1a1f1f9914264d086c
Cheolhyeon Park <fepanbr@gmail.com>
2019-11-30 10:33:27 +0900

    기능: header와 imageSlider 컴포넌트 추가
    이미지 슬라이더 컴포넌트와 헤더를 추가하였음. 이미지 슬라이더를 위해 src에 하위 디렉터리 images를 추가하여 이미지 5개 추가 (나중에 서버 구현시 서버에 이미지를 저장할 예정)

---
+++ src/components/Header.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Header.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import styled from 'styled-components';
+
+const NavbarBrand = styled(Navbar.Brand)`
+  margin-left: 15px;
+`
+
+
+function HeaderNav() {
+  return (
+    <Navbar bg="#fff" expand="lg">
+      <NavbarBrand href="#home">ITHelpU</NavbarBrand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link bsPrefix="nav-top">프로젝트</Nav.Link>
+          <Nav.Link bsPrefix="nav-top">멘토</Nav.Link>
+          <Nav.Link bsPrefix="nav-top">고민게시판</Nav.Link>
+          <Nav.Link bsPrefix="nav-top">회원가입</Nav.Link>
+          <Nav.Link bsPrefix="nav-top nav-top-end">로그인</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+export default HeaderNav;
