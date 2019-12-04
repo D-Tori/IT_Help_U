911e0617db8988970635d767abfdb43755952f5d
Cheolhyeon Park <fepanbr@gmail.com>
2019-12-04 18:08:55 +0900

    기능: react-router-dom 적용 완료 및 Home.tsx 컴포넌트 추가
    react-router-dom 적용함. app.tsx에 BrowserRouter를 적용하고 Home.tsx를 만들어 메인페이지 표현(나중에 page단위로 표현하는게 나아보임.)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -83,11 +83,11 @@ function Header() {
               <FormControl bsPrefix="search" as="input" type="text" placeholder="Search" className="mr-sm-2" />
               <Button >Search</Button>
             </Form>
-            <Nav.Link >프로젝트</Nav.Link>
-            <Nav.Link >멘토</Nav.Link>
-            <Nav.Link>고민게시판</Nav.Link>
-            <Nav.Link href="/signup">회원가입</Nav.Link>
-            <Nav.Link href="/login">로그인</Nav.Link>
+            <Nav.Item><Nav.Link >프로젝트</Nav.Link></Nav.Item>
+            <Nav.Item><Nav.Link >멘토</Nav.Link></Nav.Item>
+            <Nav.Item><Nav.Link >고민게시판</Nav.Link></Nav.Item>
+            <Nav.Item><Nav.Link href="/signup">회원가입</Nav.Link></Nav.Item>
+            <Nav.Item><Nav.Link href="/login">로그인</Nav.Link></Nav.Item>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
---
+++ src/components/Home.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from './Header';
+import ImageSlider from './ImageSlider';
+
+import Container from 'react-bootstrap/Container';
+
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <ImageSlider />
+      <Container>
+        
+      </Container>
+    </>
+  )
+}
+
+export default Home;
