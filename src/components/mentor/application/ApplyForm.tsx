0c82535a615f39b736f643a5905aafd8064f7c20
giseok <hgsok3056@naver.com>
2019-12-27 16:58:08 +0900

    기타: merge를 위한 커밋

--- src/components/mentor/application/ApplyForm.tsx
+++ src/components/mentor/application/ApplyForm.tsx
@@ -17,11 +17,11 @@ function ApplyForm(){
         </Form.Group>
         <Form.Group controlId="mentorInfo">
           <Form.Label>멘토 소개 <abbr title="required"> *</abbr></Form.Label>
-          <Form.Control as="textarea" rows="5" placeholder="현재 스펙이나 경력사항을 자세히 적어주시면 상세한 답변을 받을 수 있습니다." />
+          <Form.Control as="textarea" rows="5" placeholder="자신의 소개를 적어주세요" />
         </Form.Group>
         <Form.Group controlId="formSpec">
           <Form.Label>주요 경력 <abbr title="required"> *</abbr></Form.Label>
-          <Form.Control as="textarea" rows="5" placeholder="현재 스펙이나 경력사항을 자세히 적어주시면 상세한 답변을 받을 수 있습니다." />
+          <Form.Control as="textarea" rows="5" placeholder="주요 경력사항을 자세히 적어주세요" />
         </Form.Group>
         <Form.Group controlId="formSpec">
           <Form.Label>기타 사항 </Form.Label>
