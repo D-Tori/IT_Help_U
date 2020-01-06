beec0bde3a1c0ccdee45ab311a1542a4f31312ed
Cheolhyeon Park <fepanbr@gmail.com>
2020-01-06 16:54:28 +0900

    기능: DB연결

--- Back/index.js
+++ Back/index.js
@@ -1,12 +1,37 @@
-// require('dotenv').config()  // .env 파일 안에 있는 정보를 불러 온다.
-import dotenv from 'dotenv'; 
-dotenv.config(); // .env 파일에서 변수들을 로드한다.
+import dotenv from 'dotenv';
+import mariadb from 'mariadb';
 
-const port = process.env.PORT
+dotenv.config();
+
+const port = process.env.PORT;
 const express = require('express');
 const app = express();
 
+const pool = mariadb.createPool({
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  host: process.env.HOST,
+  port: process.env.DBPORT,
+  database: process.env.DATABASE,
+  connectionLimit: 5
+});
 
-app.get('/', (req, res) => res.send('Hello World hh'))
 
+
+async function getUserList(req, res) {
+  let conn;
+  try {
+ 
+    conn = await pool.getConnection();
+    const rows = await conn.query("select * from user");
+    console.log(rows);
+  } catch (err) {
+    throw err;
+  } finally {
+    if (conn) conn.release(); //release to pool
+  }
+}
+
+
+app.get('/', (req, res) => getUserList());
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
