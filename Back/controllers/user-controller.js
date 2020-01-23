57aedc2f513d99c4e0dde7b5f55e1caedd28803b
giseok <hgsok3056@naver.com>
2020-01-23 15:40:48 +0900

    리팩토링 : Back/ user-controller, mentor.js 수정

--- Back/controllers/user-controller.js
+++ Back/controllers/user-controller.js
@@ -88,3 +88,29 @@ exports.login = async (req, res, next) => {
   }
   
 }
+
+
+// DB에서 멘토 정보 불러오기
+const getMentorList = async () =>  {
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    const rows = await conn.query(`select * from user where role = 'mentor';`)
+    return rows;
+  } catch (err) {
+    throw err;
+  } finally {
+    if (conn) conn.release();
+  }
+}
+
+// mentors 로 오면 아래 코드가 실행된다.
+exports.mentors = (req, res) => {
+  console.log(req);
+  const rs = getMentorList();
+  rs.then((result) => {
+    console.log('result : ', result);
+    res.status(200).json(result);
+  })
+};
+
--- Back/routes/mentor.js
+++ Back/routes/mentor.js
@@ -1,31 +1,10 @@
+import userController from '../controllers/user-controller'
 import express from 'express';
-import pool from '../db/dbPool';
+
 
 const router = express.Router();
 
-
-// DB에서 멘토 정보 불러오기
-const getMentorList = async () =>  {
-  let conn;
-  try {
-    conn = await pool.getConnection();
-    const rows = await conn.query(`select * from user where role = 'mentor';`)
-    return rows;
-  } catch (err) {
-    throw err;
-  } finally {
-    if (conn) conn.release(); // release to pool
-  }
-}
-
-// mentors 로 오면 아래 코드가 실행된다.
-router.get('/', (req, res) => {
-  console.log(req);
-  const rs = getMentorList();
-  rs.then((result) => {
-    console.log('result : ', result);
-    res.status(200).json(result);
-  })
-})
+// mentors를 기본으로 들어옴
+router.get('/', userController.mentors);
 
 module.exports = router;
